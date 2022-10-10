7f23e780bb070dc08e2706df9dfa8fe4716e6571
Ajemark <83100392+Ajemark@users.noreply.github.com>
2022-10-10 10:29:38 +0100

    Update Partners.jsx

--- components/Partners/Partners.jsx
+++ components/Partners/Partners.jsx
@@ -13,7 +13,7 @@ const Partners = () => {
                     <Col lg={6} md={12} className="mx-auto">
                         <div className={`d-flex justify-content-around align-items-center`}>
                             <div className="mx-2">
-                                <Image src={require('../images/clover.jpg')} alt="reef"/>
+                                <Image height={30} src={require('../images/clover.jpg')} alt="reef"/>
                             </div>
                             <div className="mx-2">
                                  <Image src={require('../images/caduceus.png')} alt="caduceus"/>
